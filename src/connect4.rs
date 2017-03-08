23397916ba3d8698e9e5971a7025ddb7038bc476
Sunjay Varma <varma.sunjay@gmail.com>
2017-03-08 00:41:29 -0500

    Implementing more rendering and some error returning logic

--- src/connect4.rs
+++ src/connect4.rs
@@ -2,9 +2,28 @@ const ROWS: usize = 6;
 const COLS: usize = 7;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Error {
+    InvalidMove {
+        column: usize,
+    },
+    NoSpaceLeftInColumn {
+        column: usize,
+    },
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Piece {
     PieceX,
     PieceO,
+}
+
+impl Piece {
+    pub fn opposite(self) -> Piece {
+        match self {
+            Piece::PieceX => Piece::PieceO,
+            Piece::PieceO => Piece::PieceX,
+        }
+    }
 }
 
 impl Default for Piece {
@@ -31,6 +50,10 @@ impl Connect4 {
         (0..self.rows()).map(|r| self.columns.iter().map(|col| col[r]).collect()).collect()
     }
 
+    pub fn current_piece(&self) -> Piece {
+        self.current_piece
+    }
+
     pub fn rows(&self) -> usize {
         self.columns[0].len()
     }
@@ -40,6 +63,23 @@ impl Connect4 {
     }
 
     /// Drop the current piece into the given col
-    pub fn drop_piece(&mut self, col: usize) {
+    pub fn drop_piece(&mut self, col: usize) -> Result<(), Error> {
+        if col >= self.cols() {
+            return Err(Error::InvalidMove {
+                column: col,
+            });
+        }
+
+        let index = self.columns[col].iter().rposition(|t| t.is_none());
+        match index {
+            Some(index) => self.columns[col][index] = Some(self.current_piece),
+            None => return Err(Error::NoSpaceLeftInColumn {
+                column: col,
+            }),
+        }
+
+        self.current_piece = self.current_piece.opposite();
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,16 @@ use std::iter;
 
 use ansi_term::Colour::{Red, Yellow, White};
 
-use connect4::Connect4;
+use connect4::{Connect4, Piece, Error};
 use connect4::Piece::*;
 
 fn main() {
-    let game = Connect4::new();
+    let mut game = Connect4::new();
+    game.drop_piece(0).unwrap();
+    game.drop_piece(0).unwrap();
+    game.drop_piece(1).unwrap();
+    game.drop_piece(2).unwrap();
+    game.drop_piece(2).unwrap();
 
     render(game);
 }
@@ -20,23 +25,41 @@ fn render(game: Connect4) {
         format!("{}   {}", acc, s)
     }));
 
+    println!("{}  ", iter::repeat("\u{1F823}").take(7).into_iter().fold(String::new(), |acc, s| {
+        format!("{}   {}", acc, s)
+    }));
+
     for row in game.to_rows() {
+        render_line();
+
         println!("{} \u{2502}", row.iter().map(|tile| {
-            tile.map_or_else(|| {
-                White.bold().paint("\u{25CB}")
-            }, |piece| match piece {
-                PieceX => Red.bold().paint("\u{25CF}"),
-                PieceO => Yellow.bold().paint("\u{25CF}"),
-            })
+            tile.map_or_else(format_empty, format_piece)
         }).fold(String::new(), |acc, s| {
             format!("{} \u{2502} {}", acc, s)
         }));
     }
 
-    render_line();
+    render_bottom_line();
+}
+
+fn format_empty() -> String {
+    format!("{}", White.bold().paint("\u{25CB}"))
+}
+
+fn format_piece(piece: Piece) -> String {
+    format!("{}", match piece {
+        PieceX => Red.bold().paint("\u{25CF}"),
+        PieceO => Yellow.bold().paint("\u{25CF}"),
+    })
 }
 
 fn render_line() {
+    println!(" \u{253C}{}", iter::repeat("\u{2500}\u{2500}\u{2500}\u{253C}").take(7).fold(String::new(), |acc, s| {
+        acc + s
+    }));
+}
+
+fn render_bottom_line() {
     println!(" \u{2534}{}", iter::repeat("\u{2500}\u{2501}\u{2500}\u{2534}").take(7).fold(String::new(), |acc, s| {
         acc + s
     }));
